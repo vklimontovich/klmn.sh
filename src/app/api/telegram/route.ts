828c37bb8f94ace29b3db56cf04600614d2dfc3f
Vladimir Klimontovich <vladimir@jitsu.com>
2024-01-16 17:50:58 -0500

    better diagnostics

--- src/app/api/telegram/route.ts
+++ src/app/api/telegram/route.ts
@@ -105,7 +105,7 @@ export async function POST(request: NextRequest) {
     console.log(`Error handling update for ${bot.botHandle}`, e);
     await client.sendMessage(
       chatId,
-      `Internal error <code>'${e?.message || "unknown error"}'</code>. Please try again later or contact @v_klmn`
+      `Internal error <code>'${e?.message || "unknown error"}'</code>. Bot: ${botHandle}. Message: ${JSON.stringify(message, null, 2)} Please try again later or contact @v_klmn`
     );
     return new Response(e?.message || "Unknown error", { status: 500 });
   }
